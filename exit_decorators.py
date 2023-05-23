8a3cdfc7ac232d6e6635213c2add2a16cb235b18
Ken Gee <kengee47@outlook.com>
2023-05-22 19:16:16 -0700

    adding setters for ExitHandler

--- exit_decorators.py
+++ exit_decorators.py
@@ -4,19 +4,25 @@ import logging
 
 
 class ExitHandler:
-    def __init__(self, program_exit_handler=None,
-                 function_exit_handler=None,
-                 thread_exit_handler=None,
-                 logger: logging = None):
-        self.program_exit_handler = program_exit_handler
-        self.function_exit_handler = function_exit_handler
-        self.thread_exit_handler = thread_exit_handler
+    def __init__(self, logger: logging = None):
+        self.program_exit_handler = None
+        self.function_exit_handler = None
+        self.thread_exit_handler = None
         self.entity_names = {
             "program": "Program",
             "function": "Function",
             "thread": "Thread"
         }
         self.logger = logger or logging.getLogger()
+
+    def set_program_exit_handler(self, handler_func):
+        self.program_exit_handler = handler_func
+
+    def set_function_exit_handler(self, handler_func):
+        self.function_exit_handler = handler_func
+
+    def set_thread_exit_handler(self, handler_func):
+        self.thread_exit_handler = handler_func
 
     def handle_exit(self):
         if self.program_exit_handler:
@@ -38,24 +44,11 @@ class ExitHandler:
             return "thread"
 
 
-def program_exit_decorator(program_exit_handler=None):
+def exit_handler_decorator(exit_handler_func):
     def decorator(func):
-        exit_handler = ExitHandler(program_exit_handler=program_exit_handler)
+        exit_handler = ExitHandler()
+        exit_handler.set_program_exit_handler(exit_handler_func)
         atexit.register(exit_handler.handle_exit)
-        return func
-    return decorator
-
-
-def function_exit_decorator(function_exit_handler=None):
-    def decorator(func):
-        exit_handler = ExitHandler(function_exit_handler=function_exit_handler)
-        return func
-    return decorator
-
-
-def thread_exit_decorator(thread_exit_handler=None):
-    def decorator(func):
-        exit_handler = ExitHandler(thread_exit_handler=thread_exit_handler)
         return func
     return decorator
 
@@ -71,15 +64,15 @@ if __name__ == "__main__":
     def my_thread_exit_handler():
         print("Custom thread exit handler")
 
-    @program_exit_decorator(program_exit_handler=my_program_exit_handler)
+    @exit_handler_decorator(my_program_exit_handler)
     def main():
         print("Main function")
 
-    @function_exit_decorator(function_exit_handler=my_function_exit_handler)
+    @exit_handler_decorator(my_function_exit_handler)
     def my_function():
         print("My function")
 
-    @thread_exit_decorator(thread_exit_handler=my_thread_exit_handler)
+    @exit_handler_decorator(my_thread_exit_handler)
     def my_thread():
         print("My thread")
 
